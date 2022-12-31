3ccbf4e5976c28bea2b654da398219ecbefb418b
Damon <damon.j.nunez@gmail.com>
2022-12-30 19:19:54 -0500

    Official Navbar done (No Notes)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+  Navigate,
+} from "react-router-dom";
+import GiftCard from './components/GiftCard';
+import Menu from './components/Menu';
+import Home from './components/Home';
 
 function App() {
   return (
-    <Router>
       <Navbar/>
-      <Routes>
-      </Routes>
-    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,83 @@
+// import React from 'react';
+// import SBLogo from '../images/SBLOGO.webp'
+
+// import {
+//   Nav,
+//   NavLink,
+//   Bars,
+//   NavMenu,
+//   NavBtn,
+//   NavBtnLink,
+// } from './NavbarElements';
+// //Imported react tools to be used in NavBarElements.js
+  
+// const Navbar = () => {
+//   return (
+//     <>
+//       <Nav>
+//         <Bars />
+//         <img  className = "NavBarLogo"src= {SBLogo}>
+
+//         </img>
+//         <NavMenu>
+//           <NavLink to='/home' activeStyle>
+//             Home
+//           </NavLink>
+//           <NavLink to='/menu' activeStyle>
+//             Menu
+//           </NavLink>
+//           <NavLink to='/rewards' activeStyle>
+//             Rewards
+//           </NavLink>
+//           <NavLink to='/giftcards' activeStyle>
+//             GiftCards
+//           </NavLink>
+//         </NavMenu>
+//         <NavBtn>
+//           <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+//         </NavBtn>
+//         <NavBtn>
+//             <NavBtnLink to= '/login'> Log In</NavBtnLink>
+//         </NavBtn>
+//       </Nav>
+//     </>
+//   );
+// };
+  
+// export default Navbar;
 import React from 'react';
-import SBLogo from '../images/SBLOGO.webp'
+import './Navbar.css';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+function Navbar() {
+	return (
+		<div className='header'>
+			<img
+				className='header__logo'
+				src='https://upload.wikimedia.org/wikipedia/sco/d/d3/Starbucks_Corporation_Logo_2011.svg'
+				alt='starbucksheaderlogo'
+			/>
 
-import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink,
-} from './NavbarElements';
-//Imported react tools to be used in NavBarElements.js
-  
-const Navbar = () => {
-  return (
-    <>
-      <Nav>
-        <Bars />
-        <img  className = "NavBarLogo"src= {SBLogo}>
+			<div className='header__menu'>
+				<div className='header__left'>
+					<ul className='menu__items menu__left__upper'>
+						<li>menu</li>
+						<li>rewards</li>
+						<li>gift cards</li>
+					</ul>
+				</div>
 
-        </img>
-        <NavMenu>
-          <NavLink to='/home' activeStyle>
-            Home
-          </NavLink>
-          <NavLink to='/menu' activeStyle>
-            Menu
-          </NavLink>
-          <NavLink to='/rewards' activeStyle>
-            Rewards
-          </NavLink>
-          <NavLink to='/giftcards' activeStyle>
-            GiftCards
-          </NavLink>
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-        </NavBtn>
-        <NavBtn>
-            <NavBtnLink to= '/login'> Log In</NavBtnLink>
-        </NavBtn>
-      </Nav>
-    </>
-  );
-};
-  
+				<div className='header__right'>
+					<ul className='menu__items menu__items__right '>
+						<li className='findstore'>
+							<FaMapMarkerAlt className='marker' /> Find a store
+						</li>
+						<li className='list__butoons '>Sign in</li>
+						<li className='list__butoons join'>Join now</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
+
 export default Navbar;
