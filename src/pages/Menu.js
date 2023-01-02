fbed5aeaba596636356f854d995fa78bb01237e1
Damon <damon.j.nunez@gmail.com>
2023-01-02 01:03:29 -0500

    Preparing for card additions Added neccessary images

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,11 +2,15 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import TopFooter from "../components/TopFooter";
 import BottomFooter from "../components/BottomFooter";
+import CaramelCoffee from '../images/Caramel Iced Coffee.webp'
+import MochaCoconutCoffee from '../images/Mocha Coconut Chilled Coffee.webp'
+import SmoresChilledCoffee from '../images/Smores Chilled Coffee.webp'
 const Menu = () => {
     return (
         <div>
             <Navbar/>
-            <h1> This is the Menu Bitch</h1>
+             
+            
             <TopFooter/>
             <BottomFooter/>
         
