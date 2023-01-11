7913101a9ffb1277ef1e25858f9e1661b182a40f
Damon <damon.j.nunez@gmail.com>
2023-01-11 13:25:49 -0500

    Finished Side Menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -21,11 +21,21 @@ const Menu = () => {
                 <li className="item"> Iced Teas</li>
                 <li className="item"> Cold Drinks</li>
                 <h1 className="MenuSection"> Foods</h1>
-                <li className="item"> </li>
-                <li className="item"></li>
-                <li className="item"></li>
-                <li className="item"></li>
-                <li className="item"></li>
+                <li className="item"> Hot Breakfast</li>
+                <li className="item"> Bakery</li>
+                <li className="item"> Lunch</li>
+                <li className="item"> Snacks and Sweets</li>
+                <li className="item"> Oatmeal and Yogurt</li>
+                <h1 className="MenuSection"> At Home Coffee</h1>
+                <li className="item"> Whole Bean</li>
+                <li className="item"> Ground</li>
+                <li className="item"> VIA Instant</li>
+                <h1 className="MenuSection"> Merchandise</h1>
+                <li className="item"> Cold Cups</li>
+                <li className="item"> Tumblers</li>
+                <li className="item">Mugs</li>
+                <li className="item">Water Bottles</li>
+                <li className="item"> Other </li>
                </ul>
                </div>
                <div>
