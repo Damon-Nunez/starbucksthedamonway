a39313dd976999f006e1546b8c4227cfbf0ae4a8
Damon <damon.j.nunez@gmail.com>
2023-01-17 17:41:55 -0500

    Oatmeal and Yogurt

--- src/MenuChoices--Food/OatmealandYogurt.js
+++ src/MenuChoices--Food/OatmealandYogurt.js
@@ -45,13 +45,32 @@ const OatmealandYogurt = () => {
                 </div>
                <Row>
                <div className="box">
-                            <a href="">
-                                <img className="HCImage" src="">
+                            <a href="https://www.starbucks.com/menu/product/301/single">
+                                <img className="HCImage" src="https://imgs.search.brave.com/2w3l-OF1AmnBAnl1yipPea5NgKRZHliKkyqEOAnjKvM/rs:fit:305:325:1/g:ce/aHR0cDovL2dsb2Jh/bGFzc2V0cy5zdGFy/YnVja3MuY29tL2Fz/c2V0cy9kMjcxMmJh/ZmMxM2U0MjljODQy/NTIxZTI0OTE4MzU4/MC5qcGc">
 
                                 </img>
                             </a>
-                            <h1 className="caption"> </h1>
+                            <h1 className="caption">Rolled & Steel- Cut Oatmeal </h1>
                         </div>
+
+                        <div className="box">
+                            <a href="https://www.starbucks.com/menu/product/2122192/single">
+                                <img className="HCImage" src="https://imgs.search.brave.com/TVWnG_Lz29d28LfisHhP3EQTH4yiXuPxd3a-Dp_S0aY/rs:fit:400:400:1/g:ce/aHR0cHM6Ly9mYXN0/Zm9vZG51dHJpdGlv/bi5vcmcvaXRlbS1w/aG90b3MvNDAweDQw/MC85MTg0X3MuanBn">
+
+                                </img>
+                            </a>
+                            <h1 className="caption"> Berry Trio Parfait </h1>
+                        </div>
+
+                        <div className="box">
+                            <a href="https://www.starbucks.com/menu/product/2123149/single">
+                                <img className="HCImage" src="https://imgs.search.brave.com/tYZXMhc-F1uGGSv2I2csAE3thNWV1R6_Wn2HYKZCf3Q/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9mYXN0/Zm9vZG51dHJpdGlv/bi5vcmcvaXRlbS1w/aG90b3MvMTI0Mngx/MjQyLzkxODNfcy5q/cGc">
+
+                                </img>
+                            </a>
+                            <h1 className="caption"> Strawberry Overnight Grains </h1>
+                        </div>
+
                </Row>
                </Col>
                 </Row>
