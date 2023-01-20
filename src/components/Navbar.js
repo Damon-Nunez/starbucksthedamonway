1e25ec5e1fbd9c7ba0e5aa3a663defc5db37e938
Damon <damon.j.nunez@gmail.com>
2023-01-19 20:10:10 -0500

    3/4 Rewards Section Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './Navbar.css';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+
 function Navbar() {
 	return (
 		<div className='header'>
 			<Link to ="/"><img
-				
 				className='header__logo'
 				src='https://upload.wikimedia.org/wikipedia/sco/d/d3/Starbucks_Corporation_Logo_2011.svg'
 				alt='starbucksheaderlogo'
@@ -30,6 +30,7 @@ function Navbar() {
 						<li className='list__butoons '><a href='https://www.starbucks.com/account/signin'>Sign in </a></li>
 						<li className='list__butoons join'><a href='https://www.starbucks.com/account/create'>Join now </a></li>
 					</ul>
+					
 				</div>
 			</div>
 		</div>
--- src/pages/Rewards.js
+++ src/pages/Rewards.js
@@ -1,11 +1,130 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import TopFooter from "../components/TopFooter";
+import { Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import "./Rewards.css"
+
 const Rewards = () => {
     return (
         <div>
             <Navbar/>
-            <h1> This is the rewards page brodie</h1>
+            <header className="RewardsHeader"> Starbucks Rewards</header>
+            <Row className="RewardRow">
+                        <Col sm={12} md={12} lg={4} className="FreeCoffeeSection" >
+                            <div className="LightBlueSection">
+                        <h2 className="CoffeeHeader">
+                                FREE COFFEE IS A TAP AWAY
+                            </h2>
+                            <p  className="CoffeeJoin">
+                                Join now to start earning rewards 
+                            </p>
+                            <button className="JoinButton"> Join now</button>
+                            <p className="CoffeeHeaderP"> Or join in the app for the best experience </p>
+                        </div>
+                        </Col>
+                        <Col sm={12} md={12} lg={8} className="FreeCoffeeSection">
+                            <img className="Logo" src="https://imgs.search.brave.com/1Zjt6mHu0bF_L33scLWZJTnpX56FY1O-4a9rVjkzioA/rs:fit:689:690:1/g:ce/aHR0cHM6Ly93d3cu/cGluY2xpcGFydC5j/b20vcGljZGlyL2Jp/Zy8zNjYtMzY2NDQw/Nl82ODgteC03MDAt/My1zdGFyYnVja3Mt/bG9nby1wbmctdHJh/bnNwYXJlbnQucG5n"/>
+                        </Col>
+                  </Row>  
+                  <Row>
+                    <Col sm={12} md={12} lg={12}>
+                        <div className="SecondSectionHeader">
+                        <h1>Getting Started is easy </h1>
+                        <p> Earn Stars and get rewarded in a few easy steps. </p>
+                        </div>
+                    </Col>
+                    
+                    <Col sm={12} md={12} lg={4}>
+                        <div className="InfoCard">
+                        <img src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-1@2x.jpg"/>
+                        <h1> Create an account</h1>
+                        <p> To get started, <a className ="LinkWords"href="https://www.starbucks.com/account/create" >join now.</a> You can also <a className = "LinkWords" href="https://starbucks.app.link/VLa2I3inh9">join in the app </a>to get access to the full range of Starbucks® Rewards benefits.</p>
+                        </div>
+                        </Col>
+                        <Col sm={12} md={12} lg={4}>
+                        <div className="InfoCard">
+                            <img src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-2@2x.jpg"/>
+                            <h1> Order and pay how you’d like </h1>
+                            <p> Use cash, credit/debit card or save some time and pay right through the app. You’ll collect Stars all ways. <a className = "LinkWords"href ="https://www.starbucks.com/rewards#waystopay">Learn how</a></p>
+                    
+                        </div>
+                        </Col>
+                        <Col sm={12} md={12} lg={4}>
+                        <div className="InfoCard">
+                            <img src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-3@2x.jpg"/>
+                            <h1>Earn Stars, get Rewards </h1>
+                            <p> As you earn Stars, you can redeem them for Rewards—like free food, drinks, and more. Start redeeming with as little as 25 Stars! </p>
+                        </div>
+                        </Col>
+                    </Row>
+                    
+                    <Row className="RewardRowThree">
+                        <Col sm={12} md={12} lg={12}>
+                            <div className="ThirdSectionHeader">
+                                <h1> Endless Extras </h1>
+                                <p> Joining Starbucks® Rewards means unlocking access to exclusive benefits. Say hello to easy ordering, tasty Rewards and—yes, free coffee.</p>
+                            </div>
+                        </Col>
+                            <Col sm={12} md={12} lg={4}>
+                                <div className="InfoCardTwo">
+                                    <img src="https://www.starbucks.com/weblx/images/rewards/benefits/1-fun-freebies.jpg"/>
+                                    <h1>Fun freebies</h1>
+                                    <p>Not only can you earn free coffee, look forward to a birthday treat plus coffee and tea refills.</p>
+                                    <Link to = ""><h2> Learn more</h2></Link>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={12} lg={4}>
+                                <div className="InfoCardTwo">
+                                    <img src="https://www.starbucks.com/weblx/images/rewards/benefits/2-order-and-pay-ahead.jpg"/>
+                                    <h1>Order & pay ahead</h1>
+                                    <p>Enjoy the convenience of in-store, curbside or drive-thru pickup at select stores</p>
+                                    <Link to = ""><h2> Learn more</h2></Link>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={12} lg={4}>
+                            <div className="InfoCardTwo">
+                                <img src="https://www.starbucks.com/weblx/images/rewards/benefits/3-get-to-free-faster.jpg"/>
+                                <h1>Get to free faster</h1>
+                                <p>Earn Stars even quicker with Bonus Star challenges, Double Star Days and exciting games.</p>
+                                <Link to = ""><h2> Learn more</h2></Link>
+                            </div>
+                            </Col>
+                    </Row>
+                    <div className="PurpleZone">
+                    <Row className="RewardRowFour">
+                        <Col sm={12} md={12} lg={4}>
+                            <div className="FourthSectionHeader">
+                            <h1>Cash or card, you earn Stars</h1>
+                            <p> No matter how you pay, you can earn Stars with your morning coffee. Those Stars add up to (really delicious) Rewards. </p>
+                            </div>
+                        </Col>
+                        </Row>
+                        <Row className="RewardRowFive">
+                        <Col sm ={12} md={12} lg={3}>
+                            <div className="SideInfo">
+                                <h1> 1★Star per dollar </h1>
+                                <h2> Pay as you go </h2>
+                                </div>
+                        </Col>
+                        <Col sm={12} md={12} lg={4}>
+                            <div className="scanAndPay">
+                            <img src=""/>
+                            <h1>Scan and Pay Seperately </h1>
+                            <h2>Use cash or credit/debit card at the register.</h2>
+                        </div>
+                        </Col>
+                        <Col sm={12} md={12} lg={4}>
+                            <div className="savePayment">
+                            <img src=""/>
+                            <h1> Save payment in the app</h1>
+                            <h2>Check-out faster by saving a credit/debit card or PayPal to your account. You’ll be able to order ahead or scan and pay at the register in one step.</h2>
+                        </div>
+                        </Col>
+                    </Row>
+                    </div>
+                  
+               
             <TopFooter/>
         </div>
     )
